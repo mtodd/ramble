bbe73ae65df98316077ddfc4d8e11766496e28c2
Matt Todd <chiology@gmail.com>
2008-05-16 04:32:54 -0400

    Added documentation for the Post and User models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < Sequel::Model
   
+  # Returns the title as a slug-formatted string with the ID.
+  # 
+  # Examples
+  #   post.id #=> 12
+  #   post.title #=> "Hello, world!"
+  #   post.slug #=> "12-hello-world"
+  # 
+  # Returns String:slug
+  # 
   def slug
     # hugs and kisses to Rick Olson's permalink_fu
     slug = title
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < Sequel::Model
   
   class << self
     
+    # Authenticates a user against the username and password stored in the
+    # database.
+    #   +username+
+    #   +password+
+    # 
+    # Examples
+    #   User.authenticate("real_user", "correct") #=> true
+    #   User.authenticate("faker", "wrong") #=> false
+    # 
+    # Returns true|false:authenticated?
+    # 
     def authenticate(username, password)
       !self[:username => username, :password => Digest::MD5.hexdigest(password)].empty?
     end
