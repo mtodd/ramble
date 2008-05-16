4249b7e33179dd0aa5e638f0745c3209fad8dd65
Matt Todd <chiology@gmail.com>
2008-05-16 04:12:38 -0400

    Added initial model implementation (slug for Post, authenticate for User).

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < Sequel::Model
+  
+  def slug
+    # hugs and kisses to Rick Olson's permalink_fu
+    slug = title
+    slug.gsub!(/\W+/, ' ')
+    slug.strip!
+    slug.downcase!
+    slug.gsub!(/\ +/, '-')
+    return "#{id.to_s}-#{slug}"
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+require 'digest/md5'
+
 class User < Sequel::Model
+  
+  class << self
+    
+    def authenticate(username, password)
+      !self[:username => username, :password => Digest::MD5.hexdigest(password)].empty?
+    end
+    
+  end
+  
 end
