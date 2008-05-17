c0548bd13176c59b8e6cb13911c737370cf88c3f
Matt Todd <chiology@gmail.com>
2008-05-17 02:18:28 -0400

    Updated Rack initializer for 0.9.4; worked on initial layout.

--- config/router.rb
+++ config/router.rb
@@ -36,7 +36,7 @@ Merb::Router.prepare do |r|
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
   # clients from calling your create or destroy actions with a GET
-  r.default_routes
+  # r.default_routes
   
   # Change this for your home page to be available at /
   r.match('/').to(:controller => 'posts', :action =>'index')
